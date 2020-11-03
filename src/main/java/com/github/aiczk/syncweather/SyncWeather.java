0dd4fc4493cd3551e54e8158184a53b744010cbc
BelieberNeo <aiczk.398@gmail.com>
2020-11-03 22:41:12 +0900

    first commit.

---
+++ src/main/java/com/github/aiczk/syncweather/SyncWeather.java
@@ -0,0 +1,19 @@
+package com.github.aiczk.syncweather;
+
+import net.md_5.bungee.api.chat.ClickEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SyncWeather extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
