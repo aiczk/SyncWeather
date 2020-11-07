f547a6da9e83e1595299d481a86e2cd86d62d376
BelieberNeo <aiczk.398@gmail.com>
2020-11-08 02:15:28 +0900

    update.

--- src/main/kotlin/com/github/aiczk/syncweather/command/UpdateWeather.kt
+++ src/main/kotlin/com/github/aiczk/syncweather/command/UpdateWeather.kt
@@ -5,6 +5,7 @@ import com.github.aiczk.syncweather.command.json.WeatherData
 import com.github.aiczk.syncweather.util.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
 import org.bukkit.Bukkit
@@ -18,33 +19,36 @@ object UpdateWeather : CommandExecutor {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         SyncWeather.instance?.let { it ->
-            GlobalScope.launch {
-                Bukkit.getScheduler().runTaskAsynchronously(it, Runnable
-                {
-                    val locale = it.config.getString("SyncWeather-Locale")!!
-                    val isKanji = it.config.getBoolean("SyncWeather-isKanji")
+            runBlocking {
+                val syncTask = launch {
+                    Bukkit.getScheduler().runTaskAsynchronously(it, Runnable
+                    {
+                        val locale = it.config.getString("SyncWeather-Locale")!!
+                        val isKanji = it.config.getBoolean("SyncWeather-isKanji")
 
-                    val queryString = isKanji then "?stn_name_ja=$locale" ?: "?stn_name_en=$locale";
-                    val rawData = Http.get("https://jjwd.info/api/v2/stations/search$queryString")
-                    val weatherData: WeatherData? = Json.decodeFromString<WeatherData>(rawData)
+                        val queryString = isKanji then "?stn_name_ja=$locale" ?: "?stn_name_en=$locale";
+                        val rawData = Http.get("https://jjwd.info/api/v2/stations/search$queryString")
+                        val weatherData: WeatherData? = Json.decodeFromString<WeatherData>(rawData)
 
-                    weatherData?.let {
-                        val precip = it.stations?.get(0)?.preall?.precip1h!!
-                        sender.sendMessage("$precip")
-                        isStorm = precip >= 1
-                        isThundering = precip >= 5
-                    } ?: sender.sendMessage("""Error: Observatory $locale does not exist.
+                        weatherData?.let {
+                            val precip = it.stations?.get(0)?.preall?.precip1h!!
+                            sender.sendMessage("$precip")
+                            isStorm = precip >= 1
+                            isThundering = precip >= 5
+                        } ?: sender.sendMessage("""Error: Observatory $locale does not exist.
                                            |Please select the correct station name from the URL below.
                                            |https://www.jma.go.jp/jma/kishou/know/amedas/ame_master.pdf""".trimMargin())
-                })
+                    })
 
-                sender.sendMessage("ASYNC!")
+                    sender.sendMessage("ASYNC!")
+                }
+
+                syncTask.join()
+                it.server.worlds[0].setStorm(isStorm)
+                it.server.worlds[0].isThundering = isThundering
+
+                sender.sendMessage("SYNC!")
             }
-
-            it.server.worlds[0].setStorm(isStorm)
-            it.server.worlds[0].isThundering = isThundering
-
-            sender.sendMessage("SYNC!")
         }
 
         return true
