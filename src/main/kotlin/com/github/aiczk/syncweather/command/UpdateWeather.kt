89eccdef0107d5fb237253039cb2ca6d1be0a605
BelieberNeo <aiczk.398@gmail.com>
2020-11-08 03:08:58 +0900

    updated.

--- src/main/kotlin/com/github/aiczk/syncweather/command/UpdateWeather.kt
+++ src/main/kotlin/com/github/aiczk/syncweather/command/UpdateWeather.kt
@@ -2,10 +2,11 @@ package com.github.aiczk.syncweather.command
 
 import com.github.aiczk.syncweather.SyncWeather
 import com.github.aiczk.syncweather.command.json.WeatherData
-import com.github.aiczk.syncweather.util.*
+import com.github.aiczk.syncweather.util.Http
+import com.github.aiczk.syncweather.util.then
+import kotlinx.coroutines.*
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.json.Json
-import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
@@ -16,16 +17,15 @@ object UpdateWeather : CommandExecutor {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
         SyncWeather.instance?.let { it ->
-            Bukkit.getScheduler().runTaskAsynchronously(it, Runnable
-            {
+            GlobalScope.launch {
                 val locale = it.config.getString("SyncWeather-Locale")!!
                 val isKanji = it.config.getBoolean("SyncWeather-isKanji")
-
-                val queryString = isKanji then "?stn_name_ja=$locale" ?: "?stn_name_en=$locale";
-                val rawData = Http.get("https://jjwd.info/api/v2/stations/search$queryString")
-                val weatherData: WeatherData? = Json.decodeFromString<WeatherData>(rawData)
-
-                weatherData?.let {
+                withContext(Dispatchers.Default) {
+                    val queryString = isKanji then "?stn_name_ja=$locale" ?: "?stn_name_en=$locale";
+                    val rawData = Http.get("https://jjwd.info/api/v2/stations/search$queryString")
+                    val weatherJson: WeatherData? = Json.decodeFromString<WeatherData>(rawData)
+                    weatherJson
+                }?.let {
                     val precip = it.stations?.get(0)?.preall?.precip1h!!
                     sender.sendMessage("$precip")
                     isStorm = precip >= 1
@@ -34,10 +34,9 @@ object UpdateWeather : CommandExecutor {
                                            |Please select the correct station name from the URL below.
                                            |https://www.jma.go.jp/jma/kishou/know/amedas/ame_master.pdf""".trimMargin())
 
-                sender.sendMessage("ASYNC!")
                 it.server.worlds[0].setStorm(isStorm)
                 it.server.worlds[0].isThundering = isThundering
-            })
+            }
         }
 
         return true
